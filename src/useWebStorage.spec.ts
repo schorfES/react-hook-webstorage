40da20e9617142db1b46777bea5177ad45f56c8a
Norman Rusch <noreply@nrmnrsh.com>
2024-10-09 11:41:20 +0200

    Fix any typed act test function

--- src/useWebStorage.spec.ts
+++ src/useWebStorage.spec.ts
@@ -1,4 +1,5 @@
-import { act, renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 import type { StorageProvider } from './useWebStorage';
 import { useWebStorage } from './useWebStorage';
